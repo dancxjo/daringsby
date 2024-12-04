53ee072d6160223bfcffc2ba7ea8bcdfdb029182
Travis Reed <tdreed@gmail.com>
2024-12-03 17:10:30 -0800

    More botch fixes

--- lib/daringsby/network/messages/SeeMessage.ts
+++ lib/daringsby/network/messages/SeeMessage.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { Stamped } from "../../core/interfaces.ts";
-=======
->>>>>>> gapski
 import { MessageType } from "./MessageType.ts";
 import { SocketMessage } from "./SocketMessage.ts";
 
@@ -16,15 +12,3 @@ export function isValidSeeMessage(m: SocketMessage): m is SeeMessage {
     "at" in m && !!m.at && typeof m.at === "string" &&
     new Date(m.at).toString() !== "Invalid Date";
 }
-<<<<<<< HEAD
-
-export type Base64EncodedImage = string;
-
-export function stamp(m: SeeMessage): Stamped<Base64EncodedImage> {
-  return {
-    when: new Date(m.at),
-    content: m.data,
-  };
-}
-=======
->>>>>>> gapski
