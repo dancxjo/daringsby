981cad2ffd8dbad7fcffc8441410831927ae0c6b
Travis Reed <tdreed@gmail.com>
2025-06-05 17:53:41 -0700

    Update to new ip addresses (todo: remove from codebase)

--- lib/daringsby/core/core.ts
+++ lib/daringsby/core/core.ts
@@ -6,11 +6,11 @@ const forebrain = new Ollama({
   host: Deno.env.get("OLLAMA_HOST") || "http://10.0.0.180:11434",
 });
 const offload = new Ollama({
-  host: Deno.env.get("OLLAMA2_HOST") || "http://10.0.0.180:11434",
+  host: Deno.env.get("OLLAMA2_HOST") || "http://192.168.1.122:11434",
 });
 
 const slow = new Ollama({
-  host: Deno.env.get("OLLAMA3_HOST") || "http://10.0.0.180:11434",
+  host: Deno.env.get("OLLAMA3_HOST") || "http://localhost:11434",
 });
 
 export const lm = new LinguisticProcessor([forebrain, offload, slow]);
