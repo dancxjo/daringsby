c55162168e246d06a88929661565dad017885641
Travis Reed <dancxjo@users.noreply.github.com>
2025-06-08 13:38:23 -0700

    Add wit inspection route and example psyche

--- psyche/src/server.rs
+++ psyche/src/server.rs
@@ -1,4 +1,7 @@
 use crate::bus::{Event, EventBus};
+use crate::{Psyche, Sensor, Scheduler};
+use tokio::sync::Mutex;
+use serde::Serialize;
 use futures::{SinkExt, StreamExt};
 use log::info;
 use serde::Deserialize;
@@ -71,4 +74,95 @@ pub async fn run(bus: Arc<EventBus>, addr: impl Into<SocketAddr>) {
         });
 
     warp::serve(html.or(ws_route)).run(addr).await;
+}
+
+async fn wit_handler<S, P>(
+    name: String,
+    psyche: Arc<Mutex<Psyche<S, P>>>,
+) -> Result<impl warp::Reply, warp::Rejection>
+where
+    S: Scheduler + Send + Sync,
+    P: Sensor<Input = S::Output> + Send + Sync,
+    S::Output: Serialize + Clone,
+{
+    let psyche = psyche.lock().await;
+    let idx = name.parse::<usize>().ok();
+    let wit_opt = if let Some(i) = idx {
+        psyche.heart.wits.get(i)
+    } else {
+        psyche
+            .heart
+            .wits
+            .iter()
+            .find(|w| w.name.as_deref() == Some(&name))
+    };
+    if let Some(wit) = wit_opt {
+        return Ok(warp::reply::json(&wit.memory.all()));
+    }
+    Err(warp::reject::not_found())
+}
+
+/// Start the webserver with access to a [`Psyche`].
+pub async fn run_with_psyche<S, P>(
+    bus: Arc<EventBus>,
+    psyche: Arc<Mutex<Psyche<S, P>>>,
+    addr: impl Into<SocketAddr>,
+) where
+    S: Scheduler + Send + Sync + 'static,
+    P: Sensor<Input = S::Output> + Send + Sync + 'static,
+    S::Output: Serialize + Clone + Send + Sync + 'static,
+{
+
+    let html = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(warp::addr::remote())
+        .map(move |ws: warp::ws::Ws, addr: Option<SocketAddr>| {
+            let bus = bus.clone();
+            ws.on_upgrade(move |socket| handle_ws(bus, socket, addr))
+        });
+
+    let psyche_filter = warp::any().map(move || psyche.clone());
+    let wit_route = warp::path!("wit" / String)
+        .and(psyche_filter)
+        .and_then(wit_handler::<S, P>);
+
+    warp::serve(html.or(ws_route).or(wit_route)).run(addr).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::Value;
+    use warp::Reply;
+    use crate::{Experience, JoinScheduler, Sensation, Heart, Wit};
+
+    struct Echo;
+
+    impl crate::Sensor for Echo {
+        type Input = String;
+        fn feel(&mut self, s: Sensation<Self::Input>) -> Option<Experience> {
+            Some(Experience::new(s.what))
+        }
+    }
+
+    #[tokio::test]
+    async fn wit_endpoint_returns_memory() {
+        let heart = Heart::new(vec![Wit::new(JoinScheduler::default(), Echo)]);
+        let psyche = Arc::new(Mutex::new(Psyche::new(heart, vec![])));
+
+        {
+            let mut p = psyche.lock().await;
+            p.heart.wits[0].memory.remember(Sensation::new("hello".to_string()));
+        }
+
+        let resp = wit_handler::<JoinScheduler, Echo>("0".into(), psyche.clone())
+            .await
+            .unwrap();
+        let body = resp.into_response();
+        assert_eq!(body.status(), 200);
+        let bytes = warp::hyper::body::to_bytes(body.into_body()).await.unwrap();
+        let val: Value = serde_json::from_slice(&bytes).unwrap();
+        assert_eq!(val.as_array().unwrap()[0]["what"], "hello");
+    }
 }
