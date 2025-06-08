e091494747256621665cc916092e7a1b187fcb44
Travis Reed <dancxjo@users.noreply.github.com>
2025-06-08 10:15:58 -0700

    Add heart system and improve docs

--- modeldb/src/lib.rs
+++ modeldb/src/lib.rs
@@ -1,13 +1,43 @@
 use serde::{Deserialize, Serialize};
 
+/// Metadata describing an available language model.
+///
+/// ```
+/// use modeldb::{AiModel, ModelRepository};
+/// let mut repo = ModelRepository::new();
+/// repo.add_model(AiModel {
+///     name: "gpt4".into(),
+///     supports_images: true,
+///     speed: Some(1.0),
+///     cost_per_token: Some(0.02),
+/// });
+/// assert!(repo.find("gpt4").is_some());
+/// ```
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AiModel {
+    /// Unique name of the model.
     pub name: String,
+    /// Whether the model accepts image inputs.
     pub supports_images: bool,
+    /// Relative speed factor of the model.
     pub speed: Option<f32>,
+    /// Cost in dollars per generated token.
     pub cost_per_token: Option<f32>,
 }
 
+/// Simple in-memory collection of [`AiModel`]s.
+///
+/// ```
+/// use modeldb::{AiModel, ModelRepository};
+/// let mut repo = ModelRepository::new();
+/// repo.add_model(AiModel {
+///     name: "foo".into(),
+///     supports_images: false,
+///     speed: None,
+///     cost_per_token: None,
+/// });
+/// assert!(repo.find("foo").is_some());
+/// ```
 pub struct ModelRepository {
     models: Vec<AiModel>,
 }
