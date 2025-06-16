3b014dc74249d7626f63db555595f423fa2af91b
Travis Reed <dancxjo@users.noreply.github.com>
2025-06-16 12:51:00 -0700

    Replace page with simple WebSocket client

--- pete/tests/index.rs
+++ pete/tests/index.rs
@@ -3,6 +3,6 @@ use pete::index;
 #[tokio::test]
 async fn serves_index_html() {
     let resp = index().await;
-    assert!(resp.0.contains("Chat with Pete"));
+    assert!(resp.0.contains("ws://localhost:3000/ws"));
     assert!(resp.0.contains("WS:"));
 }
